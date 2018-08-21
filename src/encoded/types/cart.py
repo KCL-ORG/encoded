3f5f76c775d11307d6fcb66680213a697ee3d438
Ulugbek Baymuradov <ulugbekbk@gmail.com>
2018-08-21 16:02:40 -0700

    when in doubt, write recursion

--- src/encoded/types/cart.py
+++ src/encoded/types/cart.py
@@ -23,5 +23,18 @@ class Cart(Item):
         'type': 'array'
         })
     def items(self, request):
-        if request.datastore == 'database' and request.params.get('truncate', False):
+        truncate = is_param_in_parent(request, 'truncate')
+        if request.datastore == 'database' and truncate:
             return []
+
+
+def is_param_in_parent(request, param):
+    parent = request.__parent__
+    print(parent)
+    if parent:
+        if parent.params.get(param, None):
+            return True
+        else:
+            return is_param_in_parent(parent, param)
+    else:
+        return False
